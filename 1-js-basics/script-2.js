c70f7886ded4700ac9980f76dd0d69cd880dad95
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-01 14:12:40 +0200

    Variable Mutation and Type Coercion

---
+++ 1-js-basics/script-2.js
@@ -0,0 +1,13 @@
+// Varibales and Data Types
+
+var name = 'John';
+console.log(name);
+
+var lastName = 'Smith';
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
--- 1-js-basics/script.js
+++ 1-js-basics/script.js
@@ -1,11 +1,35 @@
+// Variable Mutation and Type Coercion
+
 var name = 'John';
-console.log(name);
-
-var lastName = 'Smith';
-console.log(lastName);
-
 var age = 26;
-console.log(age);
 
-var fullAge = true;
-console.log(fullAge);
+console.log(name + age);
+console.log(age + age);
+
+
+
+var job, isMarried;
+
+console.log(job);
+
+job = 'teacher';
+isMarried = false;
+
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married?  ' + isMarried + '.');
+
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married?  ' + isMarried + '.');
+
+
+
+// Prompt
+// var lastName = prompt('What is the last name?');
+// console.log(lastName);
+
+
+// Alert
+alert(name + ' is a ' + age + ' years old ' + job + '. Is he married?  ' + isMarried + '.');
