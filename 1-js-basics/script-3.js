eb5b962ecd4a73fa5adb238e81d00ca991546d3b
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-01 14:37:58 +0200

    Operators

---
+++ 1-js-basics/script-3.js
@@ -0,0 +1,35 @@
+// Variable Mutation and Type Coercion
+
+var name = 'John';
+var age = 26;
+
+console.log(name + age);
+console.log(age + age);
+
+
+
+var job, isMarried;
+
+console.log(job);
+
+job = 'teacher';
+isMarried = false;
+
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married?  ' + isMarried + '.');
+
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married?  ' + isMarried + '.');
+
+
+
+// Prompt
+// var lastName = prompt('What is the last name?');
+// console.log(lastName);
+
+
+// Alert
+alert(name + ' is a ' + age + ' years old ' + job + '. Is he married?  ' + isMarried + '.');
