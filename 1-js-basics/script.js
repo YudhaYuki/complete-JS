631f35ddd42d1001e456a6c6757c1b7aa1eb77ca
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-02 12:10:04 +0200

    Objects and methods

--- 1-js-basics/script.js
+++ 1-js-basics/script.js
@@ -1,38 +1,87 @@
-// First way of creating an object
+// Objects and methods
+
+/*
 var john = {
     firstName : 'John',
     lastName : 'Smith',
     yearofBirth : 1990,
     job: 'teacher',
-    isMarried: false
+    isMarried: false,
+    family: ['Jane', 'Mark', 'Bob'],
+    calculateAge : function(yearOfBirth) {
+        return 2016 - yearOfBirth;
+    }
 }
 
+console.log(john.family[2]);
+console.log(john.calculateAge(1990));
+*/
+
+
+
+
+
+
+
+
+
+// Using THIS Method
+
+// Version 1
+/*
+var john = {
+    firstName : 'John',
+    lastName : 'Smith',
+    yearOfBirth : 1990,
+    job: 'teacher',
+    isMarried: false,
+    family: ['Jane', 'Mark', 'Bob'],
+    calculateAge : function() {
+        return 2016 - this.yearOfBirth;
+    }
+}
+
+console.log(john.calculateAge());
+
+var age = john.calculateAge();
+john.age = age;
+
 console.log(john);
 
-console.log(john.lastName);
-console.log(john['lastName']);
+*/
 
-var xyz = 'job';
-console.log(john[xyz]);
 
-john.lastName = 'Miller';
-john['job'] = 'programmer';
 
+// Version 2
+
+var john = {
+    firstName : 'John',
+    lastName : 'Smith',
+    yearOfBirth : 1990,
+    job: 'teacher',
+    isMarried: false,
+    family: ['Jane', 'Mark', 'Bob'],
+    calculateAge : function() {
+        this.age = 2016 - this.yearOfBirth;
+    }
+};
+
+
+john.calculateAge();
 console.log(john);
 
 
 
 
+var mike = {
+    firstName : 'Mike',
+    yearOfBirth : 1980,
+    calculateAge : function() {
+        this.age = 2016 - this.yearOfBirth;
+    }
+};
 
 
-// Second way of creating an object
+mike.calculateAge();
+console.log(mike);
 
-var jane = new Object();
-
-jane.name = 'Jane';
-jane.lastName = 'Smith';
-jane['yearOfBirth'] = 1969;
-jane['job'] = 'retired';
-jane['isMarried'] = true;
-
-console.log(jane);
